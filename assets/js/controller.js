de9e03668304732fe538f203441cd8960d571b44
akhilkrishnang <akhilrohini@gmail.com>
2020-06-16 23:19:43 +0530

    Updated active link logic

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,7 +1,6 @@
 var app = new Vue({
-    el: '#profile',
+    el: '#profile-app',
     data: {
-        activeSection:'profile',
         mainSkills: [
             { name: 'HTML+CSS', rank: '90' },
             { name: 'Javascript', rank: '85' },
@@ -17,12 +16,29 @@ var app = new Vue({
             { name: '+' }
         ],
         subSkills: ['RESTful web services', 'LESS/SCSS', 'Agile/Scrum', 'JIRA', 'Google Material Design', 'Airbnb Design follower', 'Eclipse/STS ', 'VSCode', 'IBM CLM', 'Adobe Photoshop', 'Figma']
-    },
-    methods: {
-        goto(refName) {
-            this.activeSection = refName;
-            var element = this.$refs[refName];
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
     }
 })
+
+let mainNavLinks = document.querySelectorAll(".content__sidebar-items a");
+function updateActiveLink() {
+    let fromTop = window.scrollY;
+    let linksLength = mainNavLinks.length;
+    for (var i = 0; i < linksLength; i++) {
+        let currlink = mainNavLinks[i];
+        let currSection = document.querySelector(currlink.hash);
+        let condition = fromTop >= currSection.offsetTop;//Check if scroll position is after start of section
+
+        if (i + 1 < linksLength) {//If there is next section, modify condition
+            let nextlink = mainNavLinks[i + 1];
+            let nextSection = document.querySelector(nextlink.hash);
+            condition = condition && (fromTop < nextSection.offsetTop)//Check if scroll not passed to next section start
+        }
+
+        if (condition) {
+            currlink.classList.add("sidebar__menu-icon--active");
+        } else {
+            currlink.classList.remove("sidebar__menu-icon--active");
+        }
+    }
+}
+window.addEventListener("scroll", _.throttle(updateActiveLink, 100));
